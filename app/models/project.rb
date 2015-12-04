741bed3f8c500bf918c96eb3ffbc350216cd5e3c
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-12-04 09:47:16 -0800

    Add validations to projects and tasks

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ActiveRecord::Base
 
 	has_many :tasks
 
+	validates :name, presence: true
+
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ActiveRecord::Base
 
   belongs_to :project
+
+  validates :title, presence: true
   
 end
