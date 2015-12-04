d8b45472397b18192cf7f8b34101ecf8881b6e24
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-12-03 19:24:33 -0800

    Create project scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151204031010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.date     "ends_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
